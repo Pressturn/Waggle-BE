2cdce1ba03ccb929360bd6d38921cea3593aa9ca
Pressturn <Prestonyeo1@gmail.com>
2025-11-29 00:28:47 +0800

    Feat: verifyTOken middleware created

---
+++ src/middleware/verifyToken.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+interface AuthRequest extends Request {
+    account?: {
+        accountId: string
+    }
+}
+
+const verifyToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization
+
+    if (!token) {
+        return res.status(401).json({ error: 'No token provided' })
+    }
+
+    const tokenWithoutBearer = token.split(' ')[1]
+
+    try {
+        const decoded = jwt.verify(tokenWithoutBearer, process.env.JWT_SECTRET as string) as { accountId: string }
+        req.account = decoded
+        next()
+    } catch (error) {
+        return res.status(401).json({ error: 'Invalid Token' })
+    }
+}
+
+export default verifyToken 
