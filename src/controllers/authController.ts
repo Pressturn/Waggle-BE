8c516f353c65eea3071bc9365856b538645461bd
Pressturn <Prestonyeo1@gmail.com>
2025-11-28 18:40:56 +0800

    Feat: Added Signin route and tested

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -48,4 +48,45 @@ const signUp = async (req: Request, res: Response) => {
     }
 }
 
-export { signUp }
+const signIn = async (req: Request, res: Response) => {
+    try {
+
+        const { email, password } = req.body
+
+        const user = await prisma.user.findUnique({
+            where: { email }
+        })
+
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials' })
+        }
+
+        const correctPassword = await bcrypt.compare(password, user.password)
+
+        if (!correctPassword) {
+            return res.status(400).json({ messsage: 'Invalid credentials' })
+        }
+
+        const token = jwt.sign(
+            { userId: user.id },
+            process.env.JWT_SECRET as string,
+            { expiresIn: '6hr' }
+        )
+
+        res.json({
+            messsage: 'Login successful',
+            token,
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email
+            }
+        })
+
+    } catch (error) {
+        console.error('Signin error:', error)
+        res.status(500).json({ message: 'Serevr Error' })
+    }
+}
+
+export { signUp, signIn }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { signUp } from '../controllers/authController'
+import { signUp, signIn } from '../controllers/authController'
 
 const router = Router()
 
 router.post('/signup', signUp)
+router.post('/signin', signIn)
+
 
 export default router 
