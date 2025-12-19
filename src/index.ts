1348f990701c9694fdd9d62612dde2c1d855eaf0
Pressturn <Prestonyeo1@gmail.com>
2025-12-19 20:26:10 +0800

    Chore: route cors with origin

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,11 @@ dotenv.config()
 const app: Express = express()
 const PORT = process.env.PORT || 5001
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true
+}))
+
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
